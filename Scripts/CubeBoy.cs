7b821a450b3b9064f4e0750dfaf45597f586a1ac
Dante <hubert.dega@gmail.com>
2021-10-25 20:28:49 +0200

    Upload tutorial project

---
+++ Scripts/CubeBoy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class CubeBoy : MonoBehaviour
+{
+    StateMachine stateMachine = new StateMachine();
+    [SerializeField]
+    private LayerMask FoodItemsLayer;
+    [SerializeField]
+    private float viewRange;
+    [SerializeField]
+    string foodItemsTag;
+    NavMeshAgent NavMeshAgent;
+    private void Start()
+    {
+        this.NavMeshAgent = this.GetComponent<NavMeshAgent>();
+        this.stateMachine.ChangeState(new SearchFor(this.FoodItemsLayer, this.gameObject, this.viewRange, this.foodItemsTag, this.FoodFound ));
+    }
+    private void Update()
+    {
+        this.stateMachine.ExecuteStateUpdate();
+    }
+    public void FoodFound(SearchResults searchResult)
+    {
+        var foundFoodItems = searchResult.allHitObjectsWithRequiredTag;
+        //decide what to eat
+        //trigger eating cardridge here
+        NavMeshAgent.SetDestination(foundFoodItems[0].transform.position);
+    }
+    public void TriggerEating()
+    {
+        
+    }
+}
---
+++ Scripts/States/SearchFor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SearchFor : IState
+{
+    private LayerMask searchLayer;
+    private GameObject ownerGameObject;
+    private float searchRadius;
+    private string tagToLookFor;
+    public bool SearchCompleted;
+    private System.Action<SearchResults> searchResultCallback;
+
+    public SearchFor(LayerMask searchLayer, GameObject ownerGameObject, float searchRadius, string tagToLookFor, System.Action<SearchResults> searchResultCallback)
+    {
+        this.searchLayer = searchLayer;
+        this.ownerGameObject = ownerGameObject;
+        this.searchRadius = searchRadius;
+        this.tagToLookFor = tagToLookFor;
+        this.searchResultCallback = searchResultCallback;
+    }
+
+    public void Enter()
+    {
+        
+    }
+
+    public void Execute()
+    {
+        if (!SearchCompleted)
+        {
+            var hitObjects = Physics.OverlapSphere(this.ownerGameObject.transform.position, this.searchRadius);
+            List<Collider> allObjectsWithTheRequiredTag = new List<Collider>();
+            for (int i = 0; i < hitObjects.Length; i++)
+            {
+                if (hitObjects[i].CompareTag(this.tagToLookFor))
+                {
+                    //this.navMeshAgent.SetDestination(hitObjects[i].transform.position);
+                    allObjectsWithTheRequiredTag.Add(hitObjects[i]);
+                }
+            }
+            var searchResult = new SearchResults(hitObjects, allObjectsWithTheRequiredTag);
+            this.searchResultCallback(searchResult);
+            this.SearchCompleted = true;
+        }
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
+
+public class SearchResults
+{
+    public Collider[] allHitObjectsInSearchRadius;
+    public List<Collider> allHitObjectsWithRequiredTag;
+    //other data, like nearest/farthest object
+    public SearchResults(Collider[] allHitObjectsInSearchRadius, List<Collider> allHitObjectsWithRequiredTag)
+    {
+        this.allHitObjectsInSearchRadius = allHitObjectsInSearchRadius;
+        this.allHitObjectsWithRequiredTag = allHitObjectsWithRequiredTag;
+        //methods involving more processing,math
+    }
+}
